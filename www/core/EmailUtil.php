a64ac1d0b3452af44f2695a4a835c8d0a04b1f14
Lucas <lucas.mathieu@edu.esiee-it.fr>
2025-04-18 00:48:30 +0200

    fixed special caracters not being displayed properly in emails

--- www/core/EmailUtil.php
+++ www/core/EmailUtil.php
@@ -4,20 +4,28 @@ class EmailUtil
 {
     public static function sendVerificationEmail($to, $name, $code)
     {
-        $subject = "Verifiez votre compte";
-        $message = "Bonjour $name,\n\nVoici votre code de vérification: $code\n\nMerci de vérifier votre compte.\n\nCordialement,\nCF Project Hub";
+        $subject = "Vérifiez votre compte";
+        $message = "Bonjour $name,\n\nVoici votre code de vérification : $code\n\nMerci de vérifier votre compte.\n\nCordialement,\nCF Project Hub";
         $headers = "From: no-reply@cf-projecthub.alwaysdata.net\r\n";
-        
-        return mail($to, $subject, $message, $headers);
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+        // Encode subject for UTF-8
+        $encodedSubject = mb_encode_mimeheader($subject, "UTF-8", "B", "\r\n");
+
+        return mail($to, $encodedSubject, $message, $headers);
     }
 
     public static function sendPasswordResetEmail($to, $name, $code)
     {
         $subject = "Réinitialisez votre mot de passe";
-        $message = "Bonjour $name,\n\nVoici votre code de réinitialisation: $code\n\nMerci de réinitialiser votre mot de passe.\n\nCordialement,\nCF Project Hub";
+        $message = "Bonjour $name,\n\nVoici votre code de réinitialisation : $code\n\nMerci de réinitialiser votre mot de passe.\n\nCordialement,\nCF Project Hub";
         $headers = "From: no-reply@cf-projecthub.alwaysdata.net\r\n";
-        
-        return mail($to, $subject, $message, $headers);
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+        // Encode subject for UTF-8
+        $encodedSubject = mb_encode_mimeheader($subject, "UTF-8", "B", "\r\n");
+
+        return mail($to, $encodedSubject, $message, $headers);
     }
 }
 ?>
